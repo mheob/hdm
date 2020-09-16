2c1a2017b7e58997eb676b5de4ad6b0bf2cb59c7
Alexander Böhm <tools@boehm.work>
2020-09-16 12:19:14 +0200

    feat(purity): add the layout for mobile and desktop

--- containers/index.tsx
+++ containers/index.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout"
 import Crossflex from "@/components/Layout/Products/Crossflex"
+import Purity from "@/components/Layout/Products/Purity"
 
 const index: React.FC = () => {
   return (
     <Layout title="HDM &ndash; Holz Design Moers">
       <Crossflex />
+      <Purity />
     </Layout>
   )
 }
